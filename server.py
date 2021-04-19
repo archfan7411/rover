9238e15229190977cbf772ab09b307aeda29fb49
piesquared <40523150+piecubed@users.noreply.github.com>
2021-04-19 18:25:23 -0400

    Fix arch's terrible python

--- server.py
+++ server.py
@@ -1,32 +1,30 @@
-# aiohttp will be used to create the webserver
 from aiohttp import web
-# gpiozero's OutputDevice class is suffificient for our needs
 from gpiozero import OutputDevice
 
-# Creates the OutputDevice relay, controlling GPIO pin 17
-relay = OutputDevice(17)
 
-# Sets the pin as off. Not sure if this is required.
+PIN = 17
+relay = OutputDevice(PIN)
 relay.off()
 
-# Handles requests for the home page
+
+# Returns homepage
 async def handle(req):
     print("Got request")
-    # Returns the HTML file for viewing
     return web.FileResponse("page.html")
 
-# Handles requests to toggle the pin
+
+# Toggles the LED
 async def toggle(req):
-    # Toggles the relay
     relay.toggle()
     print(f"Toggled; Current value is {relay.value}")
     return web.Response()
 
+
+# Configure aiohttp
 app = web.Application()
+app.add_routes([
+    web.get('/toggle', toggle), 
+    web.get('/', handle),
+])
 
-# Requests to <address>/toggle should toggle, but normal
-# requests to just the address should return the home page
-app.add_routes([web.get('/toggle', toggle), web.get('/', handle)])
-
-# Starts the server, a blocking call
 web.run_app(app)
